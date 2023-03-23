c21f636610082cdf210224c886233896b1c084ae
EM40a <hermosilla.emanuel811@gmail.com>
2023-03-23 13:44:24 -0300

    add litle changes

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Edenv-Enviroment-Education
+# Edenv - Environment Education
 
-A Single Page Application about environmental care made with **HTML**, **SCSS** & **JavaScript**
+A Single Page Application about environmental care made with **HTML**, **(S)CSS** & vanilla **JavaScript**.
+
+The vectors I use are from [ManyPixels](https://www.manypixels.co/)
+
+And I use the [AOS library](https://michalsnik.github.io/aos/) for the animates on scroll
+
+Here you can see the whole page 👇
 
 ![Edenv HomePage](https://user-images.githubusercontent.com/109250897/225102159-a5c8c188-72e9-4839-9908-14314c52360a.png)
--- js/main.js
+++ js/main.js
@@ -1,30 +1,106 @@
-// Cambiar el icono
-const NAV_ICON = document.getElementById("icon-togle");
+// hamburger menu
+const hamburgerIcon = document.getElementById("icon-toggle");
 
-NAV_ICON.addEventListener("click", () => {
-    const NAV_MENU = document.getElementById("menu");
-    const NAV_OVERLAY = document.querySelector(".nav-overlay");
-    const ICON_BARS = "bars";
-    const ICON_XMARK = "xmark";
-    let iconClass = NAV_ICON.className;
-    const BODY = document.body;
-
-    NAV_OVERLAY.classList.toggle("nav-overlay--active");
-    NAV_MENU.classList.toggle("nav-menu--active");
+hamburgerIcon.addEventListener("click", () => {
+    const menu = document.getElementById("menu");
+    const overlay = document.getElementById("overlay");
+    const icon_mark = "xmark";
+    const body = document.body;
     
-    
-    if ( iconClass.includes(ICON_BARS) ) {
-        BODY.style.overflowY = 'hidden';
-        NAV_ICON.className = `fa-solid fa-${ICON_XMARK}`;
+    body.classList.toggle('active');
+    overlay.classList.toggle("active");
+    menu.classList.toggle("active");
+    hamburgerIcon.classList.toggle(`fa-${icon_mark}`);
+});
+// how to add the class active when I scroll in that section with javascript
 
+window.onload = function() {
+    // Get the current URL path
+    let path = window.location.pathname;
+  
+    // Set the active class for the matching link
+    document.querySelectorAll(`nav a[href="${path}"]`)[0].classList.add('active');
+}
+
+let navLinks = document.querySelectorAll('.menu__link');
+
+for (i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', function() {
+    // Remove the active class from all links
+    for (j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove('active');
     }
-    else if (iconClass.includes(ICON_XMARK)) {
-        BODY.style.overflowY = 'unset';
-        NAV_ICON.className = `fa-solid fa-${ICON_BARS}`;
+    // Add the active class to the clicked link
+    this.classList.add('active');
+  });
+}
+// ==============
+// validate form input
+const input = document.querySelector('input[type="email"]');
+const iconChecked = document.getElementById('icon-checked');
+let email;
 
-        NAV_MENU.classList.remove("nav-menu--active");
+const regularExpression = {
+    user: /^[a-zA-Z0-9\_\-]{4,16}$/,
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    password: /^.{4,12}$/,
+    address: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phone: /^\d{7,14}$/
+}
+
+input.addEventListener('keyup', statusValidation);
+
+function checkValidity() {
+    let emailIsValid;
+
+    email = input.value;
+
+    emailIsValid = regularExpression.address.test(email);
+
+    if (email.length <= 0) {
+        emailIsValid = null;
     }
 
+    return emailIsValid;
+}
 
+function statusValidation() {
+    if (checkValidity() === true) {
+        toggleStatus(input, true);
+        toggleStatus(iconChecked, true);
+    }
+    else if (checkValidity() === false) {
+        toggleStatus(input, false);
+        toggleStatus(iconChecked, false);
+    }
+    else {
+        input.classList.remove('valid', 'invalid')
+    }
+}
 
+function toggleStatus(e, status) {
+    if (status) {
+        if (e === input) {
+            e.classList.remove('invalid');
+            e.classList.add('valid');
+        } else {
+            e.classList.remove('fa-times-circle');
+            e.classList.add('fa-check-circle');
+        }
+    } else {
+        if (e === input) {
+            e.classList.remove('valid');
+            e.classList.add('invalid');
+        } else {
+            e.classList.remove('fa-check-circle');
+            e.classList.add('fa-times-circle');
+        }
+    }
+}
+
+// Cards animations scroll with AOS
+AOS.init({
+    once: true, // whether animation should happen only once - while scrolling down
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    delay: 400, // values from 0 to 3000, with step 50ms
 });
